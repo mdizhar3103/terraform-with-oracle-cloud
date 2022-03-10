94b7d9a5f00658ce62a6df3e50bd1b289c8a7791
mdizhar3103 <mdizhar3103@gmail.com>
2022-03-10 13:51:47 +0530

    main branch inital changes back

--- poc-terraform/comp-B/variables.tf
+++ poc-terraform/comp-B/variables.tf
@@ -27,19 +27,19 @@ variable "comp_b_vcns" {
 ########################################################
 
 variable "prod_vcn_subnets" {
-  type = map(string)
+  type        = map(string)
   description = "Prod VCN Subnets"
 }
 
 variable "nonprod_vcn_subnets" {
-  type = map(string)
+  type        = map(string)
   description = "Non-Prod VCN Subnets"
 }
 
 variable "subnet_prohibit_public_ip_on_vnic" {
-  type = bool
+  type        = bool
   description = "Prohibit Public IP on vnic"
-  default = true
+  default     = true
 }
 
 
